61976c8b63fdb0b89ece38c1e9ec58837273260c
Nir Cohen <nir0s@users.noreply.github.com>
2015-04-05 23:15:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     },
     install_requires=[
         "docopt==.0.6.1",
-        "repex==0.1.0",
+        "repex==0.1.1",
     ],
     tests_require=['nose', 'tox'],
     cmdclass={'test': Tox},
